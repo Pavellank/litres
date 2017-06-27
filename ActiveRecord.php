e0478c9bb5347f45f41df38c6c4ba9d2f57a9975
ArturTurundaev <artur.turundaev@gmail.com>
2017-06-27 19:59:44 +0300

    вторая ветвь разработки

--- ActiveRecord.php
+++ ActiveRecord.php
@@ -1,38 +1,34 @@
 <?php
 Class ActiveRecord
 {
+	private $tableName;
+	private $primaryKey;
+	public $metaData = []; //key => type
+	private static $instance = null;
+	
+	
+	public static function model()
+	{
+		if(is_null(self::$insrabce)) {
+	 		self::$instance = new self($session['pdo']);
+		}
+	
+		return self::$instance;
+	}
+	
+	
+	private function __construct(PDO $pdo)
+	{
+		$query = $this->prepare("SHOW COLUMNS FROM " . $this->tableName);
+		$query->execute();
+		$tableFieldArr = $query->fetchAll();
+	
+		foreach($tableFieldArr as $tableField) {
+			$this->metaData[$tableField['field']] = $tableField['type'];
+		}
+		
+		return $this;
+	}
 
-private $tableName;
-private $primaryKey;
 
-public $metaData =[]; //key => type
-Private static $instance = null;
-
-
-Public static model()
-{
-if(is_null(self::$insrabce)) {
- self::$instance = new self($session['pdo']);
-}
-
-Return self::$instance;
-}
-
-
-Private function __construct(PDO $pdo)
-{
-
-$q = $this->prepare("SHOW COLUMNS FROM " . $this->tableName);
-$q->execute();
-$table_fields = $q->fetchAll();
-
-Foreach($table_fields as $tableField)
-{
-$this->metaData[$tableField['field'] = $tableField['type'];
-}
-}
-
-Return $this;
-
-}
 }
