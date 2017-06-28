34119f656e3139575bd2acf9cc5009652138ef68
ArturTurundaev <artur.turundaev@gmail.com>
2017-06-28 23:10:14 +0300

    рефааакторинг

--- Const.php
+++ Const.php
@@ -4,12 +4,21 @@ class Const
 	const URL = 'я-книга.рф';
 	const PARTNER_ID = 299612548;
 	const POST_DESCRIPTION =
-		'<img src="http://'. Const::URL .'/upload/%s.jpg" alt="%s" width="228" height="368" class="aligncenter size-full" />
+		'<img src="http://'. self::URL .'/upload/%s.jpg" alt="%s" width="228" height="368" class="aligncenter size-full" />
         <strong>Автор: </strong>%s
         <strong>Название: </strong>%s
         <strong>Жанр: </strong>%s
         <strong>Язык книги: </strong>Русский
         <strong>Формат: </strong>FB2, ePub, pdf, txt и другие
 		%s
-		<a class="button green" href="%s">Скачать!</a>'; 
+		<a class="button green" href="%s">Скачать!</a>';
+
+    const POST_DOWNLOAD = 
+    '<div id="litres_trials">
+            Скачать книгу в форматах:&nbsp;
+            <a href="http://www.litres.ru/gettrial/?art=%s&format=fb2.zip&lfrom=' . self::PARTNER_ID . '" class="a-litres">FB2</a>
+            <a href="http://www.litres.ru/gettrial/?art=%s&format=epub&lfrom='    . self::PARTNER_ID . '" class="a-litres">ePUB</a>
+            <a href="http://www.litres.ru/gettrial/?art=%s&format=a4.pdf&lfrom='  . self::PARTNER_ID . '" class="a-litres">PDF</a>
+            <a href="http://www.litres.ru/gettrial/?art=%s&format=txt.zip&lfrom=' . self::PARTNER_ID . '" class="a-litres">TXT</a>
+        </div>'
 }
